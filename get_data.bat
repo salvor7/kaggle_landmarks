d5261fc3dff7a72f9e2a3da4204a152bb19acba6
Andrew Brown <salvor7@gmail.com>
2018-02-04 21:33:19 -0800

    Download all the files with one command
    I created a windows batch file for this.
    I also restructured the data folders to be under a data folder with the test,
    train and index image sets under that, and the recog and retrie data sets in
    their own folders.

---
+++ get_data.bat
@@ -0,0 +1,5 @@
+
+python src\script.py data\recognition\test.csv data\test_images\
+python src\script.py data\recognition\train.csv data\train_images\
+
+python src\script.py data\retrieval\index.csv data\index_images\
