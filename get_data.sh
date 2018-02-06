e3de5b7fffb1d4d0006e5d751f740f7b11fea7cf
Andrew Brown <salvor7@gmail.com>
2018-02-06 07:39:08 -0800

    Try getting data from bash

---
+++ get_data.sh
@@ -0,0 +1,4 @@
+
+python ./src/script.py data/retrieval/index.csv ./data/index_images/
+python ./src/script.py data/recognition/train.csv ./data/train_images/
+python ./src/script.py data/recognition/test.csv ./data/test_images/
