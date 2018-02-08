0fe899eb1f81f104c3a0ad67ccfb6418d873dc6f
Andrew Brown <salvor7@gmail.com>
2018-02-07 23:03:03 -0800

    Map image names to landmarks and vice versa
    This function creates two maps from image ID to the landmark, and
    from landmark ID to a list of image IDs.
    
    I have also memoized it, so we don't have to save these maps explicitly at run
    time.

--- src/data.py
+++ src/data.py
@@ -1,5 +1,9 @@
+import csv
 import os
 import pathlib
+from collections import defaultdict
+
+import functools
 
 
 def path_string(*args):
@@ -9,3 +13,22 @@ def path_string(*args):
     'c:\\AllProjects\\kagglelandmarks\\data\\test_images\\34983459873.jpg'
     """
     return os.path.join(str(pathlib.Path(__file__).parents[1]), 'data', *args)
+
+
+@functools.lru_cache(maxsize=1)
+def landmark_images():
+    """Returns dictionaries linking training image path strings to landmarks
+
+    :param lm: str
+    :return: [str, ...]
+    """
+    landmark2image = defaultdict(list)
+    image2landmark = {}
+
+    with open(path_string('recognition', 'train.csv')) as training_set:
+        reader = csv.reader(training_set)
+        for image, url, landmark in reader:
+            image2landmark[image] = landmark
+            landmark2image[landmark].append(image)
+
+    return landmark2image, image2landmark
